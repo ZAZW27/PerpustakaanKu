ecf1d916602aa294cec22985b24cb84bf5fbfe1a
Zharif Aziz Zulkarnain WIdodo <zharifazizzulkarnain@gmail.com>
2024-02-27 08:43:15 +0800

    dont know how many more i will be doing this but this is the newest latest ones here

--- src/view/pengguna/index.php
+++ src/view/pengguna/index.php
@@ -11,10 +11,10 @@
     ?>
     <main class="relative top-16 z-[10] px-0 md:px-3 py-4 flex flex-col justify-center items-center">
         <div class="md:w-[80vw] w-full">
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" id="datatables"     >
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
+            <div class="relative overflow-x-auto sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 shadow-md shadow-slate-400/20" id="datatables">
+                    <thead class="text-xs uppercase bg-blue-400 text-white">
+                        <tr class="border-b-[3px] border-slate-950 pt-4">
                             <th scope="col" class="px-6 py-3">
                                 Nama
                             </th>
@@ -27,8 +27,8 @@
                             <th scope="col" class="px-6 py-3">
                                 Level
                             </th>
-                            <th scope="col" class="px-6 py-3">
-                                <span class="sr-only">Action</span>
+                            <th scope="col" class="px-6 py-3 text-center">
+                                Action
                             </th>
                         </tr>
                     </thead>
@@ -37,8 +37,8 @@
                         $getUser = mysqli_query($con, "SELECT * FROM tbl_user ORDER BY level");
                         while($u = mysqli_fetch_array($getUser)){
                         ?>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                            <tr class="bg-slate-50 border-b hover:bg-slate-100">
+                                <td class="px-6 py-4 font-medium text-gray-900">
                                     <?= $u['nama_lengkap'] ?>
                                     <p class=" font-light"><?= $u['username'] ?></p>
                                 </td>
@@ -51,7 +51,7 @@
                                 <td class="px-6 py-4">
                                     <?= $u['level'] ?>
                                 </td>
-                                <th class="px-6 py-4 text-right">
+                                <td scope="col" class="px-6 py-4 text-center">
                                     <a class="font-bold" href="update.php?user=<?=$u['id_user']?>">Edit</a>
                                 </td>
                             </tr>
